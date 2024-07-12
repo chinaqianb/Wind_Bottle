fb5ecde82ecba9890b4c33f859fb8c261953721d
chinaqianb <papertyop@qq.com>
2024-07-12 17:36:32 +0800

    1.0.2,增加了一些语言

--- src/main/java/minecraftgtyf1/wind_bottle/wind_event.java
+++ src/main/java/minecraftgtyf1/wind_bottle/wind_event.java
@@ -7,14 +7,10 @@ import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
-import net.minecraft.component.Component;
-import net.minecraft.component.ComponentType;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
@@ -23,20 +19,20 @@ import org.slf4j.Logger;
 
 public class wind_event  {
     Logger log = LogUtils.getLogger();
-    public static void getEvent(World world,PlayerEntity player,Hand hand,Entity entity){
+    public static void getEvent(World world,PlayerEntity player,Hand hand,Entity entity) {
 
-            ItemStack stack = player.getStackInHand(hand);
-            if (world.isClient()){
-            if (stack.getItem()==Items.GLASS_BOTTLE) {
+        ItemStack stack = player.getStackInHand(hand);
+        if (world.isClient()) {
+            if (stack.getItem() == Items.GLASS_BOTTLE) {
                 if (entity.getType() == EntityType.BREEZE) {
                     stack.decrement(1);
                     player.giveItemStack(Wind_bottle.wind_bottle.getDefaultStack());
-                    play_wind_sound(world,player);
+                    play_wind_sound(world, player);
                 } else if (entity.getType() == EntityType.WIND_CHARGE) {
                     stack.decrement(1);
                     entity.kill();
                     player.giveItemStack(Wind_bottle.wind_bottle.getDefaultStack());
-                    play_wind_sound(world,player);
+                    play_wind_sound(world, player);
                 } else if (entity.getType() == EntityType.BREEZE_WIND_CHARGE) {
                     stack.decrement(1);
                     entity.kill();
@@ -45,8 +41,10 @@ public class wind_event  {
 
                 }
             }
+        }
 
-            }
+
+
 
     }
     public static void play_wind_sound(World world,PlayerEntity player){
