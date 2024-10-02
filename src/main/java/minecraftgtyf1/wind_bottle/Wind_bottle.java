5b2264dc796c1a1569ad57eb5deb89f62252faa0
chinaqianb <papertyop@qq.com>
2024-10-02 12:23:56 +0800

    1.1.0-beta

--- src/main/java/minecraftgtyf1/wind_bottle/Wind_bottle.java
+++ src/main/java/minecraftgtyf1/wind_bottle/Wind_bottle.java
@@ -1,18 +1,25 @@
 package minecraftgtyf1.wind_bottle;
 
+
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.entity.OminousItemSpawnerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeSerializer;
 import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.TypedActionResult;
 
 public class Wind_bottle implements ModInitializer {
     public static final Item wind_bottle =new Item(new Item.Settings().maxCount(64));
@@ -21,12 +28,18 @@ public class Wind_bottle implements ModInitializer {
     public void onInitialize() {
         Registry.register(Registries.ITEM, Identifier.of("wind_bottle","wind_bottle"),wind_bottle);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> entries.add(wind_bottle));
+
         UseEntityCallback.EVENT.register(((player, world, hand, entity, hitResult) -> {
           boolean res=  wind_event.getEvent(world,player,hand,entity);
           if (res){
               return ActionResult.SUCCESS;
           }else return ActionResult.PASS;
 
+        }));
+        UseItemCallback.EVENT.register(((player, world, hand) -> {
+            ItemStack stack =player.getStackInHand(hand);
+            boolean result =wind_event.get_OminousItem(player,world,stack);
+            return result ? TypedActionResult.success(stack) : TypedActionResult.pass(stack);
         }));
 
    }
--- src/main/java/minecraftgtyf1/wind_bottle/wind_event.java
+++ src/main/java/minecraftgtyf1/wind_bottle/wind_event.java
@@ -1,21 +1,24 @@
 package minecraftgtyf1.wind_bottle;
 
 import com.google.gson.JsonElement;
-import com.mojang.datafixers.types.Type;
-import com.mojang.datafixers.types.templates.Tag;
 import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.JsonOps;
+import minecraftgtyf1.wind_bottle.mixin.ChangeOminousItemSpawnerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.OminousItemSpawnerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import org.slf4j.Logger;
+
+import java.util.List;
 
 public class wind_event  {
     Logger log = LogUtils.getLogger();
@@ -23,6 +26,7 @@ public class wind_event  {
      boolean result =false;
         ItemStack stack = player.getStackInHand(hand);
             if (stack.getItem() == Items.GLASS_BOTTLE) {
+                
                 if (entity.getType() == EntityType.BREEZE) {
                     if (!world.isClient()) {
                     stack.decrement(1);
@@ -53,9 +57,45 @@ public class wind_event  {
                     result= true;
                 }
             }
-            return result;
+//                if (entity instanceof OminousItemSpawnerEntity){
+//
+//                    if (!world.isClient()){
+//                        player.sendMessage(Text.keybind("get it"));
+//
+//                        OminousItemSpawnerEntity ominousItemSpawner=(OminousItemSpawnerEntity) entity;
+//                       ItemStack omstack= ominousItemSpawner.getItem();
+//
+//                       player.giveItemStack(omstack);
+//                       entity.kill();
+//                    }
+//                    result=true;
+//                }
+
+        return result;
 
     }
+    public static boolean get_OminousItem(PlayerEntity player,World world,ItemStack stack){
+        boolean result=false;
+        if (stack.getItem()==Items.FLINT) {
+            List<OminousItemSpawnerEntity> omEntity = world.getEntitiesByClass(OminousItemSpawnerEntity.class, player.getBoundingBox().expand(2.0), (e) -> e != null && e.isAlive() && e instanceof OminousItemSpawnerEntity);
+            if (!omEntity.isEmpty()) {
+                OminousItemSpawnerEntity OmiE = omEntity.getFirst();
+                if (!world.isClient()) {
+                    ItemStack Omitem = OmiE.getItem();
+                    if (OmiE.age>60) {
+                        player.giveItemStack(Omitem);
+                        ((ChangeOminousItemSpawnerEntity) OmiE).setnewItem(ItemStack.EMPTY);
+                        result = true;
+                    }
+                }
+
+            }
+        }
+        return result;
+    }
+
+
+
     public static void play_wind_sound(World world,PlayerEntity player){
             world.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.NEUTRAL, 2.0F, 1.5F);
     }
